a6117452b779f5e723419701e06956cb407f519b
Kseniya357 <knesterova@gmail.com>
2019-11-24 14:13:36 -0800

    it is working

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ exports.config = {
 
 	//Capatibilities to be passed to the webdriver instance.
 	capatibilities: {
-		'browserName': 'chrome'
+		'browserName': 'chrome',
+		//chromedriver: '78.0.3904.97'
 	},
 
 	//Framework to use. Jasmin is recommended.
@@ -11,16 +12,24 @@ exports.config = {
 
 	// Specs patterns are relative to the current working directory when
 	// protractor is called.
-	specs: ['launch_wesite.js'],
+	specs: ['launch_website.js'],
 
-	onPrepare: function(){//configure junit xml report
+	// onPrepare: function(){//configure junit xml report
 
-		var jasmineReporters = require('jasmine-reporters');
-		jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
-			consolidateAll: true,
-			filePrefix: 'quitest-xmloutput',
-			savePath: 'test/reports'
-		}));
+	// 	var jasmineReporters = require('/C:/Users/User1/AppData/Roaming/npm');
+	// 	jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+	// 		consolidateAll: true,
+	// 		filePrefix: 'quitest-xmloutput',
+	// 		savePath: 'test/reports'
+	// 	}));
 
-	},
+	//},
 }
+
+  // framework: 'jasmine2',
+  //   onPrepare: function() {
+  //   var jasmineReporters = require('path_of_installed_jasmine-reporters-plugin');
+  //   //update proper path, in my case its ('/usr/local/lib/node_modules/jasmine-reporters')
+  //   jasmine.getEnv().addReporter(
+  //       new jasmineReporters.JUnitXmlReporter(null, true, true, '/test/e2e/JasmineReporter')
+  //   );};C:\Users\User1\AppData\Roaming\npm
--- launch_website.js
+++ launch_website.js
@@ -1,9 +1,11 @@
 describe('Expediate cheach vacation', function() {
 	it('This is a first test to launch the expedia website', function() {
     	browser.ignoreSynchronization = true;
+    	console.log('I am opened new bowser');
+    	browser.sleep(1000);
     	browser.get('https://www.expedia.com');
     	browser.driver.getTitle().then(function(pageTitle) {
-    		expect(pageTitle).toContain('Expedia Trevel: Search Hotels');
+    		expect(pageTitle).toContain('Expedia Travel: Search Hotels');
     	})
 	});
 });
